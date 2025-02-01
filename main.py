a6afc2d352176718b0f0f2ef23596e5d73bc1989
Guralol <138740551+Guralol@users.noreply.github.com>
2025-02-01 13:20:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *  # 导入事件类
+import ollama
+import re
+
+# 注册插件
+@register(name="Arona", description="An Arona AI", version="0.1", author="Hina")
+class MyPlugin(BasePlugin):
+
+    # 插件加载时触发
+    def __init__(self, host: APIHost):
+        pass
+
+    # 异步初始化
+    async def initialize(self):
+        pass
+
+    # 当收到个人消息时触发
+    @handler(PersonNormalMessageReceived)
+    async def person_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
+        res = ollama.chat(model="Arona", stream=False,
+                          messages=[{"role": "user", "content": msg}]).message.content
+        flag1 = re.search('</think>', res).span()[1] + 2
+        fin = res[flag1:len(res) - 1]
+         # 输出调试信息
+        self.ap.logger.debug("{}".format(ctx.event.sender_id))
+
+        # 回复消息 "hello, <发送者id>!"
+        ctx.add_return("reply", fin)
+
+        # 阻止该事件默认行为（向接口获取回复）
+        ctx.prevent_default()
+
+    # 当收到群消息时触发
+    @handler(GroupNormalMessageReceived)
+    async def group_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
+        if msg[0] == "*":
+            msg = msg[1:]
+            res = ollama.chat(model="Arona", stream=False,
+                              messages=[{"role": "user", "content": msg}]).message.content
+            flag1 = re.search('</think>', res).span()[1] + 2
+            fin = res[flag1:len(res) - 1]
+    
+            self.ap.logger.debug("{}".format(ctx.event.sender_id))
+    
+            # 回复消息 "hello, everyone!"
+            ctx.add_return("reply", fin)
+    
+            # 阻止该事件默认行为（向接口获取回复）
+            ctx.prevent_default()
+
+    # 插件卸载时触发
+    def __del__(self):
+        pass
